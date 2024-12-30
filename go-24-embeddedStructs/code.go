cb98defae241a2464a844f31aebdbc1f927527da
Mohit Kumbhare <kumbharemohit10@gmail.com>
2024-12-30 18:48:21 +0530

    Go-24-EmbeddedStructs

---
+++ go-24-embeddedStructs/code.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type car struct {
+	name  string
+	power int
+}
+
+type supercar struct {
+	car   car
+	color string
+}
+
+func main() {
+	c1 := supercar{
+		car: car{
+			name:  "Ferrari",
+			power: 800,
+		},
+		color: "blue",
+	}
+
+	fmt.Println(c1.car.name)
+}
